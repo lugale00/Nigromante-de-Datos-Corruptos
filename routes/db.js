136d5bbf97542bd09187d81a4e81a852cf07eb08
lugale00 <luis.garcialeon@alum.uca.es>
2026-03-22 13:07:23 +0100

    db_online

--- routes/db.js
+++ routes/db.js
@@ -3,10 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    port: process.env.DB_PORT || 8080,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    ssl: { rejectUnauthorized: false } // ✅ necesario para Supabase
 });
 
 module.exports = pool;
