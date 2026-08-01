8d936591352e54b86b5a01280dbe576d3264eabf
lugale00 <luis.garcialeon@alum.uca.es>
2026-08-01 11:58:38 +0200

    orden desc

--- routes/game-service.js
+++ routes/game-service.js
@@ -240,6 +240,9 @@ game.prototype.subirNivel = async function (req, res) {
 };
 
 game.prototype.comprobarSolucion = async function (req, res) {
+    const { idMision, resultadoJugador } = req.body;
+    console.log('idMision:', idMision);
+    console.log('resultadoJugador:', JSON.stringify(resultadoJugador));
     const nivelUsuario = req.session.nivelUsuario;
     if (!nivelUsuario) {
         return res.status(401).json({ error: 'No has iniciado sesión' });
